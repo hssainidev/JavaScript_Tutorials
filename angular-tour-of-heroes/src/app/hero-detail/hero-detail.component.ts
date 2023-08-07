153bd6d97284875cb41dbb9e53137d1c841bafe5
Harpreet Saini <hssaini@ualberta.ca>
2023-08-07 11:47:09 -0400

    Added dashboard capability. Updated angular packages.

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,13 +1,30 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Hero} from "../hero";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {HeroService} from "../hero.service";
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  selector: 'app-hero-detail', templateUrl: './hero-detail.component.html', styleUrls: ['./hero-detail.component.css']
 })
 
 
 export class HeroDetailComponent {
-  @Input() hero?: Hero;
+  hero: Hero | undefined;
+
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) {
+  }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  private getHero() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
 }
